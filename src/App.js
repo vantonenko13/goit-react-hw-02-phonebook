dc6dcd84f5f2485f25d1a6f92fb9813faa155c2e
Vladymyr Antonenko <vladymyr.a13@gmail.com>
2022-01-23 19:56:40 +0200

    saving contacts to localStorage

--- src/App.js
+++ src/App.js
@@ -9,14 +9,24 @@ class App extends Component {
     super();
 
     this.state = {
-      contacts: [
-        { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-        { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-        { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-        { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-      ],
+      contacts: [],
       filter: "",
     };
+  }
+
+  componentDidMount() {
+    const contacts = localStorage.getItem("contacts");
+    const parseContacts = contacts ? JSON.parse(contacts) : null;
+
+    if (parseContacts) {
+      this.setState((state) => ({ ...state, contacts: [...parseContacts] }));
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem("contacts", JSON.stringify(this.state.contacts));
+    }
   }
 
   filteredContacts = () => {
